ca23a2a6115e6049c5c8e3d4dbd31a58eed40554
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-04 16:26:34 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-//search 클래스를 클릭하면 검색창 포커스 함수 실행
+// 검색창 요소(.search) 찾기.
+const searchEl = document.querySelector('.search')
+const searchInputEl = searchEl.querySelector('input')
+// 검색창 요소를 클릭하면 실행.
 searchEl.addEventListener('click', function () {
-    searchInputEl.focus();
-});
-
-//search 포커스되면 통합 검색 placeholder를 추가
-searchInputEl,addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-//blur했을 때 통합검색 placeholder를 제거
-searchInputEl,addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
+  searchInputEl.focus()
+})
+// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused')
+  searchInputEl.setAttribute('placeholder', '통합검색')
+})
+// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused')
+  searchInputEl.setAttribute('placeholder', '')
+})
