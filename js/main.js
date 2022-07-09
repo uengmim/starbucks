0bbc891d50570aaa4ea8b77dc1d9b8e01a6402a9
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-09 11:26:27 +0900

    badge animation

--- js/main.js
+++ js/main.js
@@ -15,3 +15,24 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused')
   searchInputEl.setAttribute('placeholder', '')
 })
+
+
+const badgeEl = document.querySelector('header .badges')
+
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+// _.throttle(함수, 시간)
+window.addEventListener('scroll', _.throttle( function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    //배지 숨기기
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0
+    });
+  } else {
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1
+    });
+  }
+ }, 300))
