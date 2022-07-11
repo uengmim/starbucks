a22adbe31e38e891bca8daef83ccce4ebc8fa022
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-11 14:07:44 +0900

    image slide js

--- js/main.js
+++ js/main.js
@@ -55,3 +55,13 @@ new Swiper('.notice-line .swiper-container', {
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
 })
+
+new Swiper('.promotion .swiper-container',{
+  slidesPerView:3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 3000
+  }
+});
