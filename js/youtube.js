52868a55cb2c30d86e0a516433a6565eefd81dcc
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-13 15:23:49 +0900

    Youtube section js

--- js/youtube.js
+++ js/youtube.js
@@ -1,24 +1,28 @@
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
 
+// Youtube IFrame API를 비동기로 로드합니다.
+var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-function onYouTubeIframeAPIReady() {
-    new YT.Player('player', {
-        videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
-        playerVars: { // 영상을 재생하기 위한 여러가지 변수
-            autoplay: true, // 자동 재생 유무
-            loop: true, // 반복 재생 유무
-            playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
-        },
-        events: { //추가적인 옵션
-            onReady: function (event) { // 동영상이 준비되면 함수 실행
-                event.target.mute()  // 음소거
-            }
-        }
-    });
+// onYouTubePlayerAPIReady 함수 이름은,
+// Youtube IFrame Player API에서 사용하는 이름이기 때문에,
+// 다르게 지정하면 동작하지 않습니다!
+// 그리고 함수는 전역(Global) 등록해야 합니다!
+function onYouTubePlayerAPIReady() {
+  // <div id="player"></div>
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
+    playerVars: {
+      autoplay: true, // 자동 재생 유무
+      loop: true, // 반복 재생 유무
+      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
+    },
+    events: {
+      // 영상이 준비되었을 때,
+      onReady: function (event) {
+        event.target.mute() // 음소거!
+      }
+    }
+  })
 }
