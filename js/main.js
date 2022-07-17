84ff43c7cebf0d3d38ccec38da2545c2f28a3d4e
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-17 15:18:06 +0900

    Scroll Magic js

--- js/main.js
+++ js/main.js
@@ -98,14 +98,28 @@ function floatingObject(selector, delay, size) {
     selector, // 선택자
     random(1.5, 2.5), // 애니메이션 동작 시간
     {
-      delay: random(0, delay), // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정.
-      y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
-      repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
-      yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
-      ease: Power1.easeInOut // Easing 함수 적용.
+      delay: random(0, delay), // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정
+      y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정
+      repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복
+      yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생
+      ease: Power1.easeInOut // Easing 함수 적용
     }
   )
 }
 floatingObject('.floating1', 1, 15)
 floatingObject('.floating2', .5, 15)
 floatingObject('.floating3', 1.5, 20)
+
+// 요소가 화면에 보여짐 여부에 따른 요소 관리
+// 관리할 요소들 검색
+const spyEls = document.querySelectorAll('section.scroll-spy')
+// 요소들 반복 처리
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당
+})
