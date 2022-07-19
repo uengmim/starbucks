c15ce34e4ad8ce117efe43b2d61671d21763ed22
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-07-19 09:04:46 +0900

    Awards slide js

--- js/main.js
+++ js/main.js
@@ -48,31 +48,43 @@ fadeEls.forEach(function (fadeEl, index) {
 });
 
 
-//Notice Swiper
-//생성자(클래스)
-new Swiper('.notice-line .swiper-container', {
+/*슬라이드 요소 관리*/
+ new Swiper('.notice-line .swiper-container', {
   direction: 'vertical', // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
 })
-
-new Swiper('.promotion .swiper-container',{
-  slidesPerView:3, // 한번에 보여줄 슬라이드 개수
-  spaceBetween: 10, //슬라이드 사이 여백
+new Swiper('.promotion .swiper-container', {
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: { // 자동 재생 여부
+    delay: 5000 // 5초마다 슬라이드 바뀜
+  },
+  loop: true, // 반복 재생 여부
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
-  loop: true,
-  autoplay: {
-    delay: 3000
+  pagination: { // 페이지 번호 사용 여부
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
   },
-  pagination: {
-    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
-    clickable: true //사용자의 페이지 번호 요소 제어
-  },
-  navigation: {
-    prevEl: '.promotion .swiper-prev',
-    nextEl: '.promotion .swiper-next'
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
   }
-});
+})
+new Swiper('.awards .swiper-container', {
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+  spaceBetween: 30, // 슬라이드 사이 여백
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+  // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.awards .swiper-next' // 다음 버튼 선택자
+  }
+})
+
 
 const promotionEl = document.querySelector('.promotion'); //프로모션의 클래스를 가진 요소를 찾아서 변수 promotionEl에 넣음
 const promotionToggleBtn = document.querySelector('.toggle-promotion'); //토글 프로모션의 클래스를 가진 요소를 찾아서 변수 promotionToggleBtn에 넣음
